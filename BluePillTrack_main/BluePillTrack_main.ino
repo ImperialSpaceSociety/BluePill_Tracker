665aed70523d2f4cdfd71c2bdf474279a506d0aa
George Karfakis <george.karfakis@gmail.com>
2020-11-04 00:32:25 +0000

    Added accelerometer test

--- BluePillTrack_main/BluePillTrack_main.ino
+++ BluePillTrack_main/BluePillTrack_main.ino
@@ -1,15 +1,46 @@
+
+/*
+  Reading lat and long via UBX binary commands using UART @38400 baud - free from I2C
+  By: Nathan Seidle, Adapted from Example3_GetPosition by Thorsten von Eicken
+  SparkFun Electronics
+  Date: January 28rd, 2019
+  License: MIT. See license file for more information but you can
+  basically do whatever you want with this code.
+  This example shows how to configure the library and U-Blox for serial port use as well as
+  switching the module from the default 9600 baud to 38400.
+  Note: Long/lat are large numbers because they are * 10^7. To convert lat/long
+  to something google maps understands simply divide the numbers by 10,000,000. We 
+  do this so that we don't have to use floating point numbers.
+  Leave NMEA parsing behind. Now you can simply ask the module for the datums you want!
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun!
+  ZED-F9P RTK2: https://www.sparkfun.com/products/15136
+  NEO-M8P RTK: https://www.sparkfun.com/products/15005
+  SAM-M8Q: https://www.sparkfun.com/products/15106
+  Hardware Connections:
+  Connect the U-Blox serial TX pin to Uno pin 10
+  Connect the U-Blox serial RX pin to Uno pin 11
+  Open the serial monitor at 115200 baud to see the output
+*/
+
 #include "SparkFun_Ublox_Arduino_Library.h" //http://librarymanager/All#SparkFun_Ublox_GPS
 SFE_UBLOX_GPS myGPS;
-
 long lastTime = 0; //Simple local timer. Limits amount of I2C traffic to Ublox module.
 
 void setup()
 {
   Serial.begin(115200);
   while (!Serial); //Wait for user to open terminal
-  //Assume that the U-Blox GPS is running at 9600 baud (the default).
+  Serial.println("SparkFun Ublox Example");
+
+  //Assume that the U-Blox GPS is running at 9600 baud (the default) or at 38400 baud.
   //Loop until we're in sync and then ensure it's at 38400 baud.
   do {
+    Serial.println("GPS: trying 38400 baud");
+    Serial1.begin(38400);
+    if (myGPS.begin(Serial1) == true) break;
+
+    delay(100);
     Serial.println("GPS: trying 9600 baud");
     Serial1.begin(9600);
     if (myGPS.begin(Serial1) == true) {
@@ -17,6 +48,7 @@ void setup()
         myGPS.setSerialRate(38400);
         delay(100);
     } else {
+        //myGPS.factoryReset();
         delay(2000); //Wait a bit before trying again to limit the Serial output
     }
   } while(1);
@@ -56,3 +88,4 @@ void loop()
     Serial.println();
   }
 }
+
